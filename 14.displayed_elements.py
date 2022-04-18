115adfab1d5be5911c1478dc1c37603b4d56ba9b
TomasWettstein <tfwettstein@gmail.com>
2022-04-18 09:14:07 -0300

    Displayed_elements

---
+++ 14.displayed_elements.py
@@ -0,0 +1,20 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains   #Para hacer accion del movimiento del mouse para posicionarlo donde queramos
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+
+import time
+
+driver_path = Service('D:\\dchrome\\chromedriver_win32\\chromedriver.exe')
+browser = webdriver.Chrome(service=driver_path)
+browser.get("https://www.google.com")
+time.sleep(10)
+displayed_element = browser.find_element(by=By.NAME, value = "btnI")
+print(displayed_element.is_displayed()) #Regresa true o false si ya cargo el elemento. (Si lo mostro)
+if displayed_element.is_enabled():
+    print(displayed_element.is_enabled())#Regresa un true o false si el elemento esta disponible.(Si esta habilitado para hacer click o no.)
+    
