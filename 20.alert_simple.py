aa31013e221d3b3a90565b572518bcd2368d4288
TomasWettstein <tfwettstein@gmail.com>
2022-04-21 15:36:30 -0300

    Verificando funcionamiento de alert simple

---
+++ 20.alert_simple.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+driver_path = Service('D:\\dchrome\\chromedriver_win32\\chromedriver.exe')
+browser = webdriver.Chrome(service=driver_path)
+
+browser.get("file:///D:/Programacion/curso-python-selenium/EJEMPLOS%20ALERT/alert_simple.html")
+time.sleep(5)
+alerta = browser.find_element(by=By.ID, value = "alert")
+alerta.click()
+time.sleep(3)
+alerta = browser.switch_to.alert.accept()#El Switch tu alert, hace que se dirija a la ventana de la alerta y la acepte
+time.sleep(3)
+browser.close()
